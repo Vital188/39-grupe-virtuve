542210e99d7618eae19e589688ca268d9e951b61
Vitalij <vitalij.cernysiov@gmail.com>
2022-07-17 18:16:10 +0300

    Update

---
+++ js/Kaitlente.js
@@ -0,0 +1,31 @@
+class Kaitlente {
+    constructor(skyliuKiekis) {
+        this.size = skyliuKiekis;
+    }
+
+    render() {
+        const mainDOM = document.querySelector('main');
+
+        const columns = Math.ceil(Math.sqrt(this.size));
+        const width = 120 * columns + 2;
+        const HTML = `<div class="kaitlente" style="width: ${width}px;">
+                        <div class="top">
+                            ${'<div class="skyle"></div>'.repeat(this.size)}
+                        </div>
+                        <div class="bottom">
+                            ${'<div class="jungiklis"></div>'.repeat(this.size)}
+                        </div>
+                    </div>`;
+
+        mainDOM.innerHTML += HTML;
+    }
+
+    enableClicks() {
+        // cia aprasome veiksmu logika...
+        // susirandame skyles
+        // susirandame jungiklius
+        // kai jungiklis paspaustas: ijungiam/isjungiam atitinkama skyle
+    }
+}
+
+export { Kaitlente }
--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-console.log('labas');
+import { Kaitlente } from "./Kaitlente.js";
+
+const kaitlentciuKiekis = 5;
+
+const titleDOM = document.querySelector('.title');
+titleDOM.innerText = `Dirbtuve (${kaitlentciuKiekis})`;
+// console.log(titleDOM.textContent);
+// console.log(titleDOM.innerText);
+
+// title tampa BOLD kai: lyginis skyliu kiekis
+if (kaitlentciuKiekis % 2 === 0) {
+    titleDOM.classList.add('bold');
+}
+// title tampa RED kai: bent viena skyle karsta
+if (document.querySelector('.karsta')) {
+    titleDOM.classList.add('red');
+    titleDOM.classList.remove('title');
+
+    titleDOM.style['font-size'] = '40px';
+    // titleDOM.style['background-color'] = 'yellow';
+    titleDOM.style.backgroundColor = 'pink';
+    // titleDOM.style['border-top-style'] = 'dotted';
+    titleDOM.style.borderTopStyle = 'dotted';
+}
+
+for (let i = 1; i <= kaitlentciuKiekis; i++) {
+    const irenginys = new Kaitlente(i);
+    irenginys.render();
+    irenginys.enableClicks();
+}
+
+console.log([document.querySelector('main')]);
+document.querySelector('main').childNodes[0].remove();
